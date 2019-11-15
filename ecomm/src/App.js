dd8e7925806535bbed88a72ac574c08deb21b5d2
Navnit S <navnit_dav@yahoo.co.in>
2019-11-15 15:51:44 +0530

    new blank template for ecomm using react

---
+++ ecomm/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ECOMM APP BLANK TEMPLATE</h1>
+    </div>
+  );
+}
+
+export default App;
